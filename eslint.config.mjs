544f053dd7b9468f15969a5f033775dd356aaca6
y.smolka <yurysmolka@gmail.com>
2025-01-26 14:50:46 +0300

    change EsLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,37 +1,87 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-    // { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-    {languageOptions: { globals: {...globals.browser, __IS_DEV__: true},
-        parserOptions: { ecmaFeatures: { jsx: true } },
-        ecmaVersion: 'latest',
-        sourceType: 'module', }},
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    pluginReact.configs.flat.recommended,
-    {
-        rules: {
-            "@/indent": [2, 4],
-            'react/jsx-indent': [2, 4],
-            'react/jsx-indent-props': [2, 4],
-            'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
-            'import/no-unresolved': 'off',
-            'import/prefer-default-export': 'off',
-            'no-unused-vars': 'off',
-            'react/require-default-props': 'off',
-            'react/react-in-jsx-scope': 'off',
-            'react/jsx-props-no-spreading': 'warn',
-            'react/function-component-definition': 'off',
-            'no-shadow': 'off',
-            'import/extensions': 'off',
-            'import/no-extraneous-dependencies': 'off',
-            'no-underscore-dangle': 'off',
-            '@typescript-eslint/no-unused-vars': 'warn',
-        },
-    },
+	{ files: [`**/*.{js,mjs,cjs,ts,jsx,tsx}`] },
+	{
+		languageOptions: {
+			...pluginReact.configs.flat.recommended.languageOptions,
+			globals: { ...globals.serviceworker, ...globals.browser, __IS_DEV__: true },
+			parser: tseslint.parser,
+			parserOptions: {
+				ecmaFeatures: { jsx: true },
+				ecmaVersion: `latest`,
+				sourceType: `module`,
+			},
+		},
+		plugins: {
+			react: pluginReact
+		},
+		settings: {
+			react: {
+				createClass: `createReactClass`,
+				pragma: `React`,
+				fragment: `Fragment`,
+				version: `detect`,
+				flowVersion: `0.53`,
+			},
+			propWrapperFunctions: [
+				`forbidExtraProps`,
+				// { property: "freeze", object: "Object" },
+				// { property: "myFavoriteWrapper" },
+				// { property: "forbidExtraProps", exact: true },
+			],
+			componentWrapperFunctions: [
+				`observer`,
+				{ property: `styled` },
+				// { property: "observer", object: "Mobx" },
+				// { property: "observer", object: "<pragma>" },
+			],
+			formComponents: [
+				`CustomForm`,
+				// { name: "SimpleForm", formAttribute: "endpoint" },
+				// { name: "Form", formAttribute: ["registerEndpoint", "loginEndpoint"] },
+			],
+			linkComponents: [
+				`Hyperlink`,
+				// { name: "MyLink", linkAttribute: "to" },
+				// { name: "Link", linkAttribute: ["to", "href"] },
+			],
+		},
+	},
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	pluginReact.configs.flat.recommended,
+	pluginReact.configs.flat[`jsx-runtime`],
+	{
+		rules: {
+			'indent': [`error`, `tab`],
+			'react/jsx-indent': [`error`, `tab`],
+			'react/jsx-indent-props': [`error`, `tab`],
+			'react/jsx-filename-extension': [
+				`error`,
+				{ extensions: [`.js`, `.jsx`, `.ts`, `.tsx`] },
+			],
+			'react/jsx-curly-spacing': [2, { 'when': `always`, 'spacing': {
+				'objectLiterals': `never`
+			} }],
+			'object-curly-spacing': [`error`, `always`],
+			quotes: ['error', 'single', { 'avoidEscape': true, 'allowTemplateLiterals': true }],
+			'import/no-unresolved': `off`,
+			'import/prefer-default-export': `off`,
+			'no-unused-vars': `off`,
+			'react/require-default-props': `off`,
+			'react/react-in-jsx-scope': `off`,
+			'react/jsx-props-no-spreading': `warn`,
+			'react/function-component-definition': `off`,
+			'no-shadow': `off`,
+			'import/extensions': `off`,
+			'import/no-extraneous-dependencies': `off`,
+			'no-underscore-dangle': `off`,
+			'@typescript-eslint/no-unused-vars': `warn`,
+		},
+	},
 ];
--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -1,31 +1,31 @@
-import {classNames} from "shared/lib/classNames/classNames";
-import {useState} from "react";
-import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
-import {LangSwitcher} from "shared/ui/LangSwitcher";
-import cls from './Sidebar.module.scss'
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useState } from 'react';
+import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
+import { LangSwitcher } from 'shared/ui/LangSwitcher';
+import cls from './Sidebar.module.scss'; 
 
 interface SidebarProps {
     className?: string;
 }
 
-export const Sidebar = ({className}: SidebarProps ) => {
-    const [collapsed, setCollapsed] = useState<boolean>(false)
+export const Sidebar = ({ className }: SidebarProps ) => {
+	const [collapsed, setCollapsed] = useState<boolean>(false)
 
-    const onToggle = () => {
-        setCollapsed(prev => !prev)
-    }
+	const onToggle = () => {
+		setCollapsed(prev => !prev)
+	}
 
-    return (
-        <div className={classNames(cls.sidebar, {[cls.collapsed]: collapsed}, [className])}>
-            <button onClick={onToggle}>toggle</button>
-            <div className={cls.body}>
+	return (
+		<div className={ classNames(cls.sidebar, { [cls.collapsed]: collapsed }, [className]) }>
+			<button onClick={ onToggle }>toggle</button>
+			<div className={ cls.body }> 
 
-            </div>
+			</div>
 
-            <div className={cls.switchers}>
-                <ThemeSwitcher/>
-                <LangSwitcher/>
-            </div>
-        </div>
-    );
+			<div className={ cls.switchers }>
+				<ThemeSwitcher/> 
+				<LangSwitcher/>
+			</div>
+		</div>
+	);
 };
