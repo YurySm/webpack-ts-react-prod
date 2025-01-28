b714c0f6d26f6f2312c3a26af835c47bbafc1aa0
y.smolka <yurysmolka@gmail.com>
2025-01-28 22:27:11 +0300

    run lints

--- src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
+++ src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
@@ -4,9 +4,9 @@ import i18n from 'shared/config/i18n/i18nForTesting';
 import { render } from '@testing-library/react';
 
 export function renderWithTranslation(componet: ReactNode) {
-    return render(
-        <I18nextProvider i18n={i18n}>
-            {componet}
-        </I18nextProvider>,
-    );
+	return render(
+		<I18nextProvider i18n={ i18n }>
+			{componet}
+		</I18nextProvider>,
+	);
 }
