85e1ed3605ee0c2711ddeac93f879bcd5b1debc6
y.smolka <yurysmolka@gmail.com>
2025-02-10 18:45:07 +0300

    add user entity

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,12 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, ReducersMapObject } from '@reduxjs/toolkit';
 import { counterReducer } from 'entities/Counter';
 import { StateSchema } from './StateSchema';
+import { userReducer } from 'entities/User';
 
 export function createReduxStore(initialState: StateSchema) {
+	const rootReducer: ReducersMapObject<StateSchema> = {
+		counter: counterReducer,
+		user: userReducer,
+	};
 	return configureStore({
-		reducer: {
-			counter: counterReducer,
-		},
+		reducer: rootReducer,
 		devTools: __IS_DEV__,
 		preloadedState: initialState,
 	});
---
+++ src/entities/User/index.ts
@@ -0,0 +1,9 @@
+export {
+	userReducer,
+	userActions,
+} from './model/slice/userSlece';
+
+export {
+	User,
+	UserSchema,
+} from './model/types/user';
