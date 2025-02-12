311c2f913ca1475c5ca24564f6c15d177db50f85
y.smolka <yurysmolka@gmail.com>
2025-02-12 20:58:08 +0300

    added login form state

---
+++ src/features/AuthByUsername/model/slice/loginSlice.ts
@@ -0,0 +1,24 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { LoginSchema } from '../types/loginSchema';
+
+const initialState: LoginSchema = {
+    username: '',
+    password: '',
+    isLoading: false,
+};
+
+export const loginSlice = createSlice({
+    name: 'login',
+    initialState,
+    reducers: {
+        setUsername: (state, { payload }: PayloadAction<string>) => {
+            state.username = payload;
+        },
+        setPassword: (state, { payload }: PayloadAction<string>) => {
+            state.password = payload;
+        },
+    },
+});
+
+export const { actions: loginActions } = loginSlice;
+export const { reducer: loginReducer } = loginSlice;
--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -3,6 +3,10 @@ import { useTranslation } from 'react-i18next';
 import { Input } from 'shared/ui/Input/Input';
 import cls from './LoginForm.module.scss';
 import { Button } from 'shared/ui/Button/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { getLoginState } from '../../model/selectors/getLoginState';
+import { useCallback } from 'react';
+import { loginActions } from '../../model/slice/loginSlice';
 
 interface LoginFormProps {
     className?: string;
@@ -10,10 +14,30 @@ interface LoginFormProps {
 
 export const LoginForm = ({ className }: LoginFormProps) => {
 	const { t } = useTranslation();
+	const dispatch = useDispatch();
+	const { username, password } = useSelector(getLoginState);
+
+	const changeUsername = useCallback((value: string) => {
+		dispatch(loginActions.setUsername(value));
+	}, [dispatch]);
+
+	const changePassword = useCallback((value: string) => {
+		dispatch(loginActions.setPassword(value));
+	}, [dispatch]);
+
 	return (
 		<div className={ classNames(cls.loginForm, {}, [className]) }>
-			<Input autofocus placeholder={ t('Логин') } />
-			<Input placeholder={ t('Пароль') }  />
+			<Input
+				onChange={ changeUsername }
+				autofocus
+				placeholder={ t('Логин') }
+				value={ username }
+			/>
+			<Input
+				onChange={ changePassword }
+				placeholder={ t('Пароль') }
+				value={ password }
+			/>
 			<Button>
 				{t('Войти')}
 			</Button>
