49de29f08faf25fa0f5ac57c010468458aeb741b
y.smolka <yurysmolka@gmail.com>
2025-02-20 21:15:23 +0300

    change ProfilePage.stories.ts

--- src/pages/ProfilePage/ui/ProfilePage.stories.ts
+++ src/pages/ProfilePage/ui/ProfilePage.stories.ts
@@ -2,11 +2,13 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/decorators/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
 import ProfilePage from './ProfilePage';
+import { StoreDecorator } from 'shared/config/storybook/decorators/StoreDecorator';
 
 const meta: Meta<typeof ProfilePage> = {
 	title: 'pages/ProfilePage',
 	component: ProfilePage,
 	argTypes: {},
+	decorators: [StoreDecorator({})]
 };
 
 export default meta;
@@ -17,5 +19,6 @@ export const Light: Story = {};
 export const Dark: Story = {
 	decorators: [
 		ThemeDecorator(Theme.DARK),
+
 	],
 };
