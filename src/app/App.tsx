f6fa31c737432a9c99d62293c3e4226d0afbdb66
y.smolka <yurysmolka@gmail.com>
2025-02-21 23:23:18 +0300

    add init user auth data

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,9 +3,17 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
+import { useAppDispatch } from 'app/providers/StoreProvider/config/store';
+import { useEffect } from 'react';
+import { userActions } from 'entities/User';
 
 export const App = () => {
 	const { theme } = useTheme();
+	const dispatch = useAppDispatch();
+
+	useEffect(() => {
+		dispatch(userActions.initAuthData());
+	}, [dispatch]);
 
 	return (
 		<div className={ classNames('app', {}, [theme]) }>
