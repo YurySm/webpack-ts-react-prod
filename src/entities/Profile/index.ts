65bf64703abe95d813f68656d5c41b174f50bdf9
y.smolka <yurysmolka@gmail.com>
2025-02-24 22:19:22 +0300

    added fetchProfileDate.ts

--- src/entities/Profile/index.ts
+++ src/entities/Profile/index.ts
@@ -1,4 +1,5 @@
-// export { Profile } from './ui/Profile/Profile'
 export { Profile, ProfileSchema } from './model/types/profile';
 
 export { profileActions, profileReducer } from './model/slice/profileSlice';
+
+export { fetchProfileData } from './model/services/fetchProfileDate/fetchProfileDate';
