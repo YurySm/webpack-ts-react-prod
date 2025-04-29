b160a19634b189fa83e231014acf10d5a81c5d2e
y.smolka <yurysmolka@gmail.com>
2025-04-29 08:46:07 +0300

    added tabs component

--- src/shared/ui/Card/Card.tsx
+++ src/shared/ui/Card/Card.tsx
@@ -2,20 +2,27 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Card.module.scss';
 import { HTMLAttributes, ReactNode } from 'react';
 
+export enum CardTheme {
+    NORMAL = 'normal',
+    OUTLINED = 'outlined',
+}
+
 interface CardProps extends HTMLAttributes<HTMLDivElement> {
     className?: string;
     children?: ReactNode;
+    theme?: CardTheme;
 }
 
 export const Card = (props: CardProps) => {
     const {
         className,
         children,
+        theme = CardTheme.NORMAL,
         ...otherProps
     } = props;
     return (
         <div
-            className={ classNames(cls.card, {}, [className]) }
+            className={ classNames(cls.card, {}, [cls[theme], className]) }
             { ...otherProps }
         >
             {children}
---
+++ src/shared/ui/Tabs/Tabs.tsx
@@ -0,0 +1,48 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './Tabs.module.scss';
+import { ReactNode, useCallback } from 'react';
+import { Card, CardTheme } from 'shared/ui/Card/Card';
+
+interface TabItem {
+    value: string;
+    content: ReactNode;
+}
+
+interface TabsProps {
+    className?: string;
+    tabs: TabItem[];
+    value: string;
+    onTabClick: (tab: TabItem) => void;
+}
+
+export const Tabs = (props: TabsProps) => {
+    const {
+        className,
+        tabs,
+        value,
+        onTabClick,
+    } = props;
+
+    const clickHandler = useCallback((tab: TabItem) => {
+        return () => {
+            onTabClick(tab)
+        }
+    }, [onTabClick])
+
+    return (
+        <div className={ classNames(cls.tabs, {}, [className]) }>
+            {
+                tabs.map(tab => (
+                    <Card
+                        theme={ tab.value === value ? CardTheme.OUTLINED : CardTheme.NORMAL }
+                        key={ tab.value }
+                        className={ cls.tab }
+                        onClick={ clickHandler(tab) }
+                    >
+                        {tab.content}
+                    </Card>
+                ))
+            }
+        </div>
+    );
+};
