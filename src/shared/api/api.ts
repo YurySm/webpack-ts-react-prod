2619d619bd4eed1a3059a35292bed6124914a88a
y.smolka <yurysmolka@gmail.com>
2025-05-13 09:00:38 +0300

    changed

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,7 +3,12 @@ import { USER_LOCAL_STORAGE_KEY } from 'shared/constants/localstorage';
 
 export const $api = axios.create({
     baseURL: __API__,
-    headers: {
-        Authorization: localStorage.getItem(USER_LOCAL_STORAGE_KEY) || '',
-    },
 });
+
+$api.interceptors.request.use(config => {
+    if(config.headers) {
+        config.headers.Authorization = localStorage.getItem(USER_LOCAL_STORAGE_KEY) || ''
+    }
+
+    return config;
+})
