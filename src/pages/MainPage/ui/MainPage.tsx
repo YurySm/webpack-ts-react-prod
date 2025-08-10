d773ab149257434952d5568f8e6937547fe95653
y.smolka <yurysmolka@gmail.com>
2025-08-10 21:08:05 +0300

    added stars rating entity

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,17 +1,11 @@
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/widgets/Page/ui/Page';
-import { Rating } from '@/entities/Rating';
 
 const MainPage = () => {
     const { t } = useTranslation('main');
     return (
         <Page>
             {t('Главная')}
-            <Rating
-                title={ 'Как вам статья?' }
-                hasFeedback
-                feedbackTitle={ 'Оставьте отзыв' }
-            />
         </Page>
     )
 };
